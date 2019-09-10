4678fb0dc322d0e878b91aee5e0452b6b406e3b9
Ryan Jacobs <ryan@ryanjjacobs.com>
2019-09-10 08:19:10 -0600

    change logger level; undo change to null

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
